dae6a2939294a06abf9c19a3a9bc3a94a6ad7944
Jörg Kütemeier <joerg@kuetemeier.de>
2023-01-27 22:43:38 +0100

    Telescope: sharpen keyboard mappings

--- profiles/telescope.nix
+++ profiles/telescope.nix
@@ -15,7 +15,7 @@
 #
 # }}}
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   config = {
@@ -23,14 +23,16 @@
 
     maps.normal = {
       "<leader>ff" = "<cmd>Telescope find_files<cr>";
+      "<c-o>" = "<cmd>Telescope find_files<cr>";
+
       "<leader>fg" = "<cmd>Telescope live_grep<cr>";
       "<leader>fb" = "<cmd>Telescope buffers<cr>";
       "<leader>fh" = "<cmd>Telescope help_tags<cr>";
+      "<leader>fk" = "<cmd>Telescope keymaps<cr>";
+      "<leader>fc" = "<cmd>Telescope commands<cr>";
+      "<leader>f/" = "<cmd>Telescope search_history<cr>";
 
-      "<c-p>" = "<cmd>Telescope find_files<cr>";
-      "<c-s-p>" = "<cmd>Telescope commands<cr>";
-      "<c-k>" = "<cmd>Telescope buffers<cr>";
-      "<c-s-k>" = "<cmd>Telescope keymaps<cr>";
+      "<leader>?" = "<cmd>Telescope keymaps<cr>";
     };
   };
 }
