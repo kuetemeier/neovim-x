1899bab4b2831d9b03f4498cfd5440a2e0e5723f
Jörg Kütemeier <joerg@kuetemeier.de>
2023-01-31 22:01:15 +0100

    Keymaps: Fix formatting add more key mappings

--- profiles/keymaps.nix
+++ profiles/keymaps.nix
@@ -1,11 +1,10 @@
-
 #  ## Key mappings
 #
 #  (from: https://raw.githubusercontent.com/pta2002/nixvim/main/README.md )
 #
 #  It is fully possible to define key mappings from within NixVim. This is done
 #  using the `maps` attribute:
-#  
+#
 #  ```nix
 #  {
 #    programs.nixvim = {
@@ -19,16 +18,16 @@
 #    };
 #  }
 #  ```
-#  
+#
 #  This is equivalent to this vimscript:
-#  
+#
 #  ```vim
 #  noremap ; :
 #  nnoremap <leader>m <silent> <cmd>make<CR>
 #  ```
-#  
+#
 #  This table describes all modes for the `maps` option:
-#  
+#
 #  | NixVim         | NeoVim                                           |
 #  |----------------|--------------------------------------------------|
 #  | normal         | Normal mode                                      |
@@ -42,10 +41,10 @@
 #  | insertCommand  | Insert and command-line mode                     |
 #  | lang           | Insert, command-line and lang-arg mode           |
 #  | command        | Command-line mode                                |
-#  
+#
 #  The map options can be set to either a string, containing just the action,
 #  or to a set describing additional options:
-#  
+#
 #  | NixVim  | Default | VimScript                                |
 #  |---------|---------|------------------------------------------|
 #  | silent  | false   | `<silent>`                               |
@@ -55,15 +54,11 @@
 #  | unique  | false   | `<unique>`                               |
 #  | noremap | true    | Use the 'noremap' variant of the mapping |
 #  | action  | N/A     | Action to execute                        |
-
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   config = {
-
     # set <leader> before everything else
     extraConfigLuaPre = ''
-      vim.g.mapleader = " " 
+      vim.g.mapleader = " "
     '';
 
     maps = {
@@ -84,24 +79,35 @@
           silent = true;
           action = "<cmd>NvimTreeFindFile<CR>";
         };
-
       };
 
+      # Make sure leader does not do it's default action
+      normal."<Space>" = "<Nop>";
+
       # use alt+hjkl to move between split/vsplit panels
-      normal."<A-h>" = { action = "<C-w>h"; };
-      normal."<A-j>" = { action = "<C-w>j"; };
-      normal."<A-k>" = { action = "<C-w>k"; };
-      normal."<A-l>" = { action = "<C-w>l"; };
-      terminal."<A-h>" = { action = "<C-\><C-n><C-w>h"; };
-      terminal."<A-j>" = { action = "<C-\><C-n><C-w>j"; };
-      terminal."<A-k>" = { action = "<C-\><C-n><C-w>k"; };
-      terminal."<A-l>" = { action = "<C-\><C-n><C-w>l"; };
+      normal."<A-h>" = {action = "<C-w>h";};
+      normal."<A-j>" = {action = "<C-w>j";};
+      normal."<A-k>" = {action = "<C-w>k";};
+      normal."<A-l>" = {action = "<C-w>l";};
+      terminal."<A-h>" = {action = "<C-\><C-n><C-w>h";};
+      terminal."<A-j>" = {action = "<C-\><C-n><C-w>j";};
+      terminal."<A-k>" = {action = "<C-\><C-n><C-w>k";};
+      terminal."<A-l>" = {action = "<C-\><C-n><C-w>l";};
 
       # resize windows (horizontal splits)
-      normal."<leader>+" = { action = "<cmd>exe \"resize \" . (winheight(0) * 3/2)<CR>"; };
-      normal."<leader>-" = { action = "<cmd>exe \"resize \" . (winheight(0) * 2/3)<CR>"; };
+      normal."<leader>+" = {action = "<cmd>exe \"resize \" . (winheight(0) * 3/2)<CR>";};
+      normal."<leader>-" = {action = "<cmd>exe \"resize \" . (winheight(0) * 2/3)<CR>";};
+
+      # Indent, stay in visual mode
+      visual."<" = "<gv";
+      visual.">" = ">gv";
+
+      # Move text under cursor up and down
+      visual."<A-j>" = ":m .+1<CR>==";
+      visual."<A-k>" = ":m .-2<CR>==";
+
+      # When pasting in visual mode, do not yank the replaces text
+      visual."p" = "\"_dP";
     };
   };
-
 }
-
