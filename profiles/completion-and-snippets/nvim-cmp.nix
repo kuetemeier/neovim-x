adec9cfc3114cf4d9264056e9ed10a87f35cc4fa
Jörg Kütemeier <joerg@kuetemeier.de>
2023-01-31 22:35:49 +0100

    Completion: Fine-Tuning (better <CR> handling, add <S-TAB> etc.)

--- profiles/completion-and-snippets/nvim-cmp.nix
+++ profiles/completion-and-snippets/nvim-cmp.nix
@@ -13,50 +13,98 @@
 #
 # }}}
 {...}: {
-  config.plugins.nvim-cmp = {
-    enable = true;
+  config = {
+    plugins.nvim-cmp = {
+      enable = true;
 
-    sources = [
-      {name = "buffer";}
-      {name = "path";}
-      {name = "nvim_lsp";}
-      {name = "latex_symbols";}
-      {name = "emoji";}
-      {name = "cmdline";}
-      # {name = "luasnip";} # added by luasnip.nix
-    ];
+      sources = [
+        {name = "buffer";}
+        {name = "path";}
+        {name = "nvim_lsp";}
+        {name = "latex_symbols";}
+        {name = "emoji";}
+        {name = "cmdline";}
+        # {name = "luasnip";} # added by luasnip.nix
+      ];
 
-    mappingPresets = ["insert"];
+      preselect = "None";
 
-    mapping = {
-      "<CR>" = "cmp.mapping.confirm({ select = true })";
-      "<Tab>" = ''
-        cmp.mapping(function(fallback)
-          local luasnip=require("luasnip")
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-           fallback()
-          end
-        end, {"i", "s"})'';
-    };
+      mappingPresets = ["insert" "cmdline"];
 
-    formatting.fields = ["kind" "abbr" "menu"];
+      mapping = {
+        # Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        "<CR>" = "cmp.mapping.confirm({ select = false })";
 
-    window.completion = {
-      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
-      col_offset = -4;
-      side_padding = 0;
-      border = "single";
-    };
+        "<C-e" = "cmp.mapping.abort()";
 
-    window.documentation = {
-      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
-      border = "single";
+        "<Tab>" = {
+          modes = ["i" "s"];
+          action = ''
+            function(fallback)
+              local luasnip=require("luasnip")
+              if luasnip.jumpable(1) then
+                luasnip.jump(1)
+              elseif cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expandable() then
+                luasnip.expand()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              elseif check_backspace() then
+                fallback()
+              else
+                fallback()
+              end
+            end
+          '';
+        };
+
+        "<S-Tab>" = {
+          modes = ["i" "s"];
+          action = ''
+            function(fallback)
+              local luasnip=require("luasnip")
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end
+          '';
+        };
+
+        "<C-b>" = ''cmp.mapping.scroll_docs(-4)'';
+
+        "<C-f>" = ''cmp.mapping.scroll_docs(4)'';
+
+        "<C-Space>" = ''
+          cmp.mapping.complete({
+            config = {
+              sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "path" },
+                { name = "buffer" },
+              }
+            }
+          })'';
+      };
+
+      formatting.fields = ["kind" "abbr" "menu"];
+
+      window.completion = {
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
+        col_offset = -4;
+        side_padding = 0;
+        border = "single";
+      };
+
+      window.documentation = {
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
+        border = "single";
+      };
     };
   };
 }
